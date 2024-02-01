b01b38a785e161b5325f8325fcc17e4436d052e2
yenaing-oo <64188092+yenaing-oo@users.noreply.github.com>
2024-02-01 01:21:25 -0600

    set up project folder

---
+++ app/src/main/java/comp3350/teachreach/application/MainActivity.java
@@ -0,0 +1,16 @@
+package comp3350.teachreach.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.teachreach.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
