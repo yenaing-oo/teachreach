49939f9c6aebde7ee2971fd409ec113a08d98862
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-02-05 05:20:51 +0000

    Update file Session.java

--- app/src/main/Objects/Session.java
+++ app/src/main/Objects/Session.java
@@ -1,1 +1,37 @@
+public class Session {
+    private Student student;
+    private Tutor tutor;
+    private int[] date
+    private int hour;
+    private boolean accepted;
 
+    public Session(Student student, Tutor tutor, int day, int month, int year, int hour) {
+        this.student = student;
+        this.tutor = tutor;
+        this.date = {day, month, year};
+        this.hour = hour;
+        this.accepted = false;
+    }
+
+    public void acceptSession() {
+        this.accepted = true;
+    }
+    
+    public Student getStudent() {
+        return this.student;
+    }
+
+    public Tutor getTutor() {
+        return this.tutor;
+    }
+
+    public int[] getDate() {
+        return this.date;
+    }
+
+    public void setDate(int day, int month, int year) {
+        this.date = {date,month,year}
+    }
+
+    
+}
