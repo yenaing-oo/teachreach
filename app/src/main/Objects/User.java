6c84424865936d5993cb1b2f28e3dd88ab63e6fc
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-02-05 05:13:17 +0000

    Upload New File

---
+++ app/src/main/Objects/User.java
@@ -0,0 +1,37 @@
+public class User extends Account {
+    private String name;
+    private String pronouns;
+    private String major;
+
+    public User(String name, String pronouns, String major, String email, String password) {
+        super(email, password);
+        this.name = name;
+        this.pronouns = pronouns;
+        this.major = major;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPronouns() {
+        return this.pronouns;
+    }
+
+    public void setPronouns(String pronouns) {
+        this.pronouns = pronouns;
+    }
+
+    public String getMajor() {
+        return this.major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+}
