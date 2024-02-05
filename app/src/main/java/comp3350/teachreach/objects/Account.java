00f8b8eb1870ab4624d0e7245d0aa13aaa34c036
yenaing-oo <64188092+yenaing-oo@users.noreply.github.com>
2024-02-04 20:23:40 -0600

    added DSO objects

---
+++ app/src/main/java/comp3350/teachreach/objects/Account.java
@@ -0,0 +1,27 @@
+package comp3350.teachreach.objects;
+
+public class Account {
+    private String email;
+    private String password;
+
+    public Account(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/comp3350/teachreach/objects/Student.java
@@ -0,0 +1,8 @@
+package comp3350.teachreach.objects;
+
+public class Student extends User {
+
+    public Student(String name, String pronouns, String major, String email, String password) {
+        super(name, pronouns, major, email, password);
+    }
+}
