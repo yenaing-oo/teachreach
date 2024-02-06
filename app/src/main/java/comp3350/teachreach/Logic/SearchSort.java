82c58ea46b861ced6a0b3be328e742b09695fa2e
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-02-06 03:48:36 +0000

    Update file SearchSort.java

--- app/src/main/java/comp3350/teachreach/Logic/SearchSort.java
+++ app/src/main/java/comp3350/teachreach/Logic/SearchSort.java
@@ -23,6 +23,52 @@ public class SearchSort {
     }
 
     public ArrayList<Tutor> sortByRating(ArrayList<Tutor> tutors) {
+        return ratingMergeSort(tutors);
+    }
+
+    private ArrayList<Tutor> ratingMergeSort(ArrayList<Tutor> tutors) {
+        if(tutors.size() <= 1) {
+            return tutors;
+        }
+        else {
+            int middle = (tutors.size() - 1)/2;
+            ArrayList<Tutor> left = ratingMergeSort(tutors.subList(0,middle));
+            ArrayList<Tutor> right = ratingMergeSort(middle+1, tutors.size()-1)
+            return ratingMerge(left, right);
+        }
+    }
+
+    private ArrayList<Tutor> ratingMerge(<ArrayList<Tutor> left, ArrayList<Tutor> right) {
+        int leftCount = 0;
+        int rightCount = 0;
+        ArrayList<Tutor> out = new ArrayList<Tutor>();
+
+        while(leftCount < left.size() && rightCount < right.size()) {
+            if(right[rightCount].getAverageScore() > left[leftScore].getAverageScore()) {
+                out.add(right[rightCount]);
+                rightCount++;
+            }
+            else {
+                out.add(left[leftCount]);
+                leftCount++;
+            }
+        }
+        while(leftCount < left.size()) {
+            out.add(left[leftCount]);
+            leftCount++;
+        }
+        while(rightCount < right.size()) {
+            out.add(right[rightCount]);
+                rightCount++;
+        }
+
+        return out;
+    }
+
+    public ArrayList<Tutor> sortByPrice(ArrayList<Tutor> tutors) {
         
     }
-}
+
+    
+ 
+ }
