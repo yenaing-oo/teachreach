5db2d7d4f7c6f8bbd2f99d23ea820a3f4ce0dde8
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-02-06 03:26:07 +0000

    Update file SearchSort.java

--- app/src/main/java/comp3350/teachreach/Logic/SearchSort.java
+++ app/src/main/java/comp3350/teachreach/Logic/SearchSort.java
@@ -1,1 +1,28 @@
 
+import comp3350.teachreach.objects;
+
+import java.util.ArrayList;
+
+public class SearchSort {
+
+    public ArrayList<Tutor> searchTutorClass(ArrayList<Tutor> tutors, Course course) {
+        ArrayList<Tutor> output = new ArrayList<Tutor>();
+        boolean flag = false;
+        for(int i=0; i<tutors.size(); i++) {
+            ArrayList<Course> tutored = tutors[i].getCourses
+            flag = false;
+            for(int j=0; j<tutored.size(); j++) {
+                flag |= tutored[j].equals(course);
+            }
+            if(flag) {
+                output.add(tutors[i]);
+            }
+        }
+        
+        return output;
+    }
+
+    public ArrayList<Tutor> sortByRating(ArrayList<Tutor> tutors) {
+        
+    }
+}
