b608a6376c30419c08682959c6c66c68351c9b3c
Rafia <islamrr@myumanitoba.ca>
2024-02-08 00:58:44 -0600

    Login is connected to Logic

--- app/src/main/java/comp3350/teachreach/presentation/TutorLoginActivity.java
+++ app/src/main/java/comp3350/teachreach/presentation/TutorLoginActivity.java
@@ -1,23 +1,25 @@
 package comp3350.teachreach.presentation;
 
-//package comp3350.teachreach.application;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import comp3350.teachreach.R;
+import comp3350.teachreach.logic.LoginHandler;
+import comp3350.teachreach.objects.AccountType;
 
 public class TutorLoginActivity extends AppCompatActivity {
 
     private EditText etTutorEmail, etTutorPassword;
     private Button btnLogin;
     private TextView tvSignUp;
+    private LoginHandler loginHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +31,45 @@ public class TutorLoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnTutorLogin);
         tvSignUp = findViewById(R.id.tvTutorSignUp);
 
+        loginHandler = new LoginHandler();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //login logic for tutor
-                // login();
+                login();
             }
         });
 
         tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navigate to Tutor Sign Up Activity
                 Intent intent = new Intent(TutorLoginActivity.this, TutorSignUpActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void login() {
+        String email = etTutorEmail.getText().toString().trim();
+        String password = etTutorPassword.getText().toString().trim();
+
+        if (validateInputs(email, password)) {
+            if (loginHandler.validateCredential(AccountType.Tutor, email, password)) {
+
+                Intent intent = new Intent(TutorLoginActivity.this, TutorProfileActivity.class);
+                startActivity(intent);
+                finish(); // Close the current activity
+            } else {
+                Toast.makeText(this, "Invalid email or password. Please try again.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean validateInputs(String email, String password) {
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Email and password cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
