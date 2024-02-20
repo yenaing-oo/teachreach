e1b04e2167e99273d9fad6010550612f0760f128
chinweimak <makc1@myumanitoba.ca>
2024-02-19 19:44:41 -0600

    Test

---
+++ app/src/test/java/comp3350/teachreach/tests/object/SessionTest.java
@@ -0,0 +1,97 @@
+package comp3350.teachreach.tests.object;
+
+import comp3350.teachreach.objects.Session;
+import comp3350.teachreach.objects.Student;
+import comp3350.teachreach.objects.Tutor;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SessionTest {
+/*
+    public void testCreateSession(){
+        Session session = new Session(students.get(1), tutors.get(3), 26, 5, 11, 3,"Library");
+    }
+
+
+    public Session(Student student, Tutor tutor, int day, int month, int year, int hour, String location) {
+        this.student = student;
+        this.tutor = tutor;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hour = hour;
+        this.accepted = false;
+        this.location = location;
+    }
+
+    public void acceptSession() {
+        this.accepted = true;
+    }
+
+    public Student getStudent() {
+        return this.student;
+    }
+
+    public void setStudent(Student newstudent) {
+        this.student = newstudent;
+    }
+
+    public Tutor getTutor() {
+        return this.tutor;
+    }
+
+    public void setTutor(Tutor newtutor) {
+        this.tutor = newtutor;
+    }
+
+
+    public int getDay() {
+        return this.day;
+    }
+    public int getMonth() {
+        return this.month;
+    }
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setDay(int newDay) {
+        this.day = newDay;
+    }
+
+    public void setMonth(int newMonth) {
+        this.month = newMonth;
+    }
+
+    public void setYear(int newYear) {
+        this.year = newYear;
+    }
+
+    public void setHour(int newHour) {
+        this.hour = newHour;
+    }
+
+    public boolean getStage() {
+        return this.accepted;
+    }
+
+    public void setStage(boolean decision) {
+        this.accepted = decision;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getHour() {
+        return this.hour;
+    }
+    */
+
+}
