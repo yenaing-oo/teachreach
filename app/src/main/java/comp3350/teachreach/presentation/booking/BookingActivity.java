13321759cdde3b2420ab01ef446bb58863a7d759
yenaing-oo <64188092+yenaing-oo@users.noreply.github.com>
2024-03-03 22:13:01 -0600

    displays selected date from Calendar in BookingActivity

--- app/src/main/java/comp3350/teachreach/presentation/booking/BookingActivity.java
+++ app/src/main/java/comp3350/teachreach/presentation/booking/BookingActivity.java
@@ -1,19 +1,26 @@
 package comp3350.teachreach.presentation.booking;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import comp3350.teachreach.R;
+import comp3350.teachreach.utils.DateUtils;
 
 public class BookingActivity extends AppCompatActivity implements OnDateChangeListener {
     private FragmentManager fragmentManager;
+    private TextView dateDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        dateDisplay = findViewById(R.id.dateDisplayTextView);
+        dateDisplay.setVisibility(View.GONE);
 
         fragmentManager = getSupportFragmentManager();
         addDateSelectionFragment();
@@ -38,6 +45,8 @@ public class BookingActivity extends AppCompatActivity implements OnDateChangeLi
 
     @Override
     public void onDateChanged(int year, int month, int dayOfMonth) {
+        dateDisplay.setText(DateUtils.formatDate(year, month, dayOfMonth));
+        dateDisplay.setVisibility(View.VISIBLE);
         addTimeSlotSelectionFragment();
     }
 }
---
+++ app/src/main/java/comp3350/teachreach/utils/DateUtils.java
@@ -0,0 +1,24 @@
+package comp3350.teachreach.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DateUtils {
+    public static String formatDate(int year, int month, int dayOfMonth) {
+        // Construct a date representation from the selected year, month, and day
+        Calendar selectedDate = Calendar.getInstance();
+        selectedDate.set(year, month, dayOfMonth);
+
+        return dateFormatHelper(selectedDate);
+    }
+
+    public static String formatDate(Calendar selectedDate) {
+        return dateFormatHelper(selectedDate);
+    }
+
+    private static String dateFormatHelper(Calendar date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, MMM d", Locale.getDefault());
+        return sdf.format(date.getTime());
+    }
+}
