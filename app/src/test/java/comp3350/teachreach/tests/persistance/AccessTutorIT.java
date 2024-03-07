e73d89316138b530b6219db207635c62c9ffc19f
chinweimak <makc1@myumanitoba.ca>
2024-03-07 02:57:03 -0600

    Itest -ACCESS(init tutor, student)  implementation @Chin

--- app/src/test/java/comp3350/teachreach/tests/persistance/AccessTutorIT.java
+++ app/src/test/java/comp3350/teachreach/tests/persistance/AccessTutorIT.java
@@ -1,6 +1,29 @@
 package comp3350.teachreach.tests.persistance;
 
+import org.junit.Before;
+
+import java.io.IOException;
+
+import comp3350.teachreach.data.hsqldb.TutorHSQLDB;
+import comp3350.teachreach.data.interfaces.ITutorPersistence;
+import comp3350.teachreach.logic.DAOs.AccessStudents;
+import comp3350.teachreach.logic.DAOs.AccessTutors;
+import comp3350.teachreach.tests.utils.TestUtils;
+
 public
 class AccessTutorIT
 {
+    private AccessStudents accessTutors;
+    @Before
+    public
+    void setUp() throws IOException
+    {
+        this.tempDB = TestUtils.copyDB();
+        final ITutorPersistence persistence = new TutorHSQLDB(this.tempDB
+                .getAbsolutePath()
+                .replace(
+                        ".script",
+                        ""));
+        this.accessTutors = new AccessTutors(persistence);
+    }
 }
