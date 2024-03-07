48b5bfb5164409ac6cc07b477998978e4d69e4b9
hokt <hokt@myumanitoba.ca>
2024-03-06 19:17:58 -0600

    updated session stub xx <3

--- app/src/main/java/comp3350/teachreach/data/stubs/SessionStub.java
+++ app/src/main/java/comp3350/teachreach/data/stubs/SessionStub.java
@@ -1,70 +1,25 @@
 package comp3350.teachreach.data.stubs;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
-import comp3350.teachreach.data.interfaces.IAccountPersistence;
 import comp3350.teachreach.data.interfaces.ISessionPersistence;
-import comp3350.teachreach.data.interfaces.IStudentPersistence;
-import comp3350.teachreach.data.interfaces.ITutorPersistence;
 import comp3350.teachreach.objects.Session;
 import comp3350.teachreach.objects.TimeSlice;
 import comp3350.teachreach.objects.interfaces.ISession;
-import comp3350.teachreach.objects.interfaces.IStudent;
-import comp3350.teachreach.objects.interfaces.ITutor;
 
 public
 class SessionStub implements ISessionPersistence
 {
-
-    List<ISession>      sessions;
-    int                 sessionIDCounter;
-    IAccountPersistence accountsDataAccess;
-    IStudentPersistence studentsDataAccess;
-    ITutorPersistence   tutorsDataAccess;
-
-    public
-    SessionStub()
-    {
-        sessions         = new ArrayList<>();
-        sessionIDCounter = 0;
-
-        accountsDataAccess = new AccountStub();
-        studentsDataAccess = new StudentStub();
-        tutorsDataAccess   = new TutorStub(accountsDataAccess);
-    }
-
-    @Override
-    public
-    ISession storeSession(IStudent theStudent,
-                          ITutor theTutor,
-                          TimeSlice sessionTime,
-                          String location)
-    {
-        ISession newSession = new Session(theStudent,
-                                          theTutor,
-                                          sessionTime,
-                                          location);
-        return newSession.setSessionID(sessionIDCounter++);
-    }
-
-    @Override
-    public
-    boolean deleteSession(ISession session)
-    {
-        return sessions.removeIf(otherSession -> session.getSessionID() ==
-                                                 otherSession.getSessionID());
-    }
+    private static Map<Integer, ISession> sessions;
+    private static int                    sessionCount = 1;
 
     /**
-     * @param studentID
-     * @param tutorID
-     * @param sessionTime
-     * @param location
-     * @return
+     * @param studentID   the student's id
+     * @param tutorID     the tutor's id
+     * @param sessionTime a TimeSlice consist of start and end Instant
+     * @param location    location set for this session
+     * @return the new session stored with an assigned sessionID
      */
     @Override
     public
@@ -73,23 +28,33 @@ class SessionStub implements ISessionPersistence
                           TimeSlice sessionTime,
                           String location)
     {
-        return null;
+        ISession newSession = sessions.put(sessionCount,
+                                           new Session(sessionCount,
+                                                       studentID,
+                                                       tutorID,
+                                                       sessionTime,
+                                                       location));
+        sessionCount++;
+        return newSession;
     }
 
     /**
-     * @param newSession
-     * @return
+     * @param newSession a new ISession object to be inserted
+     * @return the newSession stored with an assigned sessionID
      */
     @Override
     public
     ISession storeSession(ISession newSession)
     {
-        return null;
+        return storeSession(newSession.getSessionStudentID(),
+                            newSession.getSessionTutorID(),
+                            newSession.getTime(),
+                            newSession.getSessionLocation());
     }
 
     /**
-     * @param sessionID
-     * @return
+     * @param sessionID ID of session to be deleted
+     * @return if deletion is successful
      */
     @Override
     public
@@ -98,71 +63,29 @@ class SessionStub implements ISessionPersistence
         return false;
     }
 
+    /**
+     * @param existingSession an existing session to be updated
+     * @return the updated session object
+     */
     @Override
     public
-    boolean updateSession(ISession session)
+    ISession updateSession(ISession existingSession)
     {
-        ISession updatedSession = null;
-        boolean  result         = false;
-        for (ISession s : sessions) {
-            if (s.getSessionID() == session.getSessionID()) {
-                s.setSessionLocation(session.getAtLocation());
-                s.setStudent(session.getStudent());
-                s.setTutor(session.getTutor());
-                s.setStage(session.getStage());
-                updatedSession = s;
-                result         = true;
-                break;
-            }
-        }
-        if (updatedSession == null) {
+        if (SessionStub.sessions.containsKey(existingSession.getSessionID())) {
+            return SessionStub.sessions.put(existingSession.getSessionID(),
+                                            existingSession);
+        } else {
             throw new NoSuchElementException();
         }
-        return result;
     }
 
     /**
-     * @return
+     * @return a Map of Integer sessionID -> ISession objects
      */
     @Override
     public
     Map<Integer, ISession> getSessions()
     {
-        return null;
-    }
-
-    @Override
-    public
-    List<ISession> getSessionsByRangeForStudent(int StudentAID, TimeSlice range)
-    {
-        return sessions
-                .stream()
-                .filter(session -> range.canContain(session.getTime()) &&
-                                   session.getStudent().getAccountID() ==
-                                   StudentAID)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public
-    List<ISession> getSessionsByRangeForTutor(int StudentAID, TimeSlice range)
-    {
-        return sessions
-                .stream()
-                .filter(session -> range.canContain(session.getTime()) &&
-                                   session.getTutor().getAccountID() ==
-                                   StudentAID)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public
-    List<ISession> getPendingSessionRequests(int AID)
-    {
-        return sessions
-                .stream()
-                .filter(session -> session.getTutor().getAccountID() == AID &&
-                                   session.getStage())
-                .collect(Collectors.toList());
+        return SessionStub.sessions;
     }
 }
