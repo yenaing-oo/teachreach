a23945744c835cd4c16a315077ba1513cc6349d6
yenaing-oo <64188092+yenaing-oo@users.noreply.github.com>
2024-03-13 16:28:06 -0500

    refactor

--- app/src/main/java/comp3350/teachreach/application/TRData.java
+++ app/src/main/java/comp3350/teachreach/application/TRData.java
@@ -16,11 +16,7 @@ class TRData
     {
         try {
             Class.forName("org.hsqldb.jdbcDriver").newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         dbName = name;
