684df104a428f407efdba3fca671d179f7951c2d
hokt <hokt@myumanitoba.ca>
2024-03-16 21:36:01 -0500

    New tutor login ui

--- app/src/main/java/comp3350/teachreach/presentation/login/TutorLoginActivity.java
+++ app/src/main/java/comp3350/teachreach/presentation/login/TutorLoginActivity.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import comp3350.teachreach.R;
 import comp3350.teachreach.logic.account.AuthenticationHandler;
@@ -16,47 +17,48 @@ import comp3350.teachreach.objects.interfaces.ITutor;
 import comp3350.teachreach.presentation.profile.TutorProfileActivity;
 import comp3350.teachreach.presentation.signup.TutorSignUpActivity;
 
-public
-class TutorLoginActivity extends AppCompatActivity
+public class TutorLoginActivity extends AppCompatActivity
 {
-
+    private TextInputLayout tilTutorEmail, tilTutorPassword;
     private EditText etTutorEmail, etTutorPassword;
     private AuthenticationHandler authenticationHandler;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_login);
 
-        etTutorEmail = findViewById(R.id.etTutorEmail);
-        etTutorPassword = findViewById(R.id.etTutorPassword);
-        Button btnLogin = findViewById(R.id.btnTutorLogin);
-        TextView tvSignUp = findViewById(R.id.tvTutorSignUp);
+        tilTutorEmail    = findViewById(R.id.tilTutorLoginEmail);
+        tilTutorPassword = findViewById(R.id.tilTutorLoginPassword);
+        etTutorEmail     = tilTutorEmail.getEditText();
+        etTutorPassword  = tilTutorPassword.getEditText();
+        Button btnLogin  = findViewById(R.id.btnLoginAsTutor);
+        Button btnSignUp = findViewById(R.id.btnSignupAsTutor);
 
         authenticationHandler = new AuthenticationHandler();
 
         btnLogin.setOnClickListener(v -> login());
 
-        tvSignUp.setOnClickListener(v -> {
+        btnSignUp.setOnClickListener(v -> {
             Intent intent = new Intent(TutorLoginActivity.this,
-                    TutorSignUpActivity.class);
+                                       TutorSignUpActivity.class);
             startActivity(intent);
         });
     }
 
-    private
-    void login()
+    private void login()
     {
         String email    = etTutorEmail.getText().toString().trim();
         String password = etTutorPassword.getText().toString().trim();
         try {
             InputValidator.validateEmail(email);
             InputValidator.validatePassword(password);
-            ITutor tutor = authenticationHandler.authenticateTutor(email, password);
-
+            ITutor tutor = authenticationHandler.authenticateTutor(email,
+                                                                   password);
 
             Intent intent = new Intent(TutorLoginActivity.this,
-                    TutorProfileActivity.class);
+                                       TutorProfileActivity.class);
             intent.putExtra("TUTOR_ID", tutor.getTutorID());
             startActivity(intent);
             finish();
