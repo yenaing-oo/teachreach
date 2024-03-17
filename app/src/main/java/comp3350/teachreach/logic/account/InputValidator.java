c6cfa3244a7e9666dc2f433402634ba3d432ab46
hokt <hokt@myumanitoba.ca>
2024-03-16 22:22:48 -0500

    Updated login activity UI with material stuff

--- app/src/main/java/comp3350/teachreach/logic/account/InputValidator.java
+++ app/src/main/java/comp3350/teachreach/logic/account/InputValidator.java
@@ -7,34 +7,35 @@ import comp3350.teachreach.logic.exceptions.InvalidNameException;
 import comp3350.teachreach.logic.exceptions.input.InvalidEmailException;
 import comp3350.teachreach.logic.exceptions.input.InvalidPasswordException;
 
-public
-class InputValidator {
-    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_" +
-            "`{|}~-]+(?:\\" +
-            ".[a-z0-9!#$%&'*+/=?^_" +
-            "`{|}~-]+)*|\"" +
-            "(?:[\\x01-\\x08\\x0b\\x0c" +
-            "\\x0e-\\x1f\\x21\\x23-\\x5b" +
-            "\\x5d-\\x7f]|\\\\[\\x01" +
-            "-\\x09\\x0b\\x0c\\x0e-\\x7f" +
-            "])*\")@(?:(?:[a-z0-9]" +
-            "(?:[a-z0-9-]*[a-z0-9])?\\.)" +
-            "+[a-z0-9](?:[a-z0-9-]*[a-z0" +
-            "-9])?|\\[(?:(?:(2" +
-            "(5[0-5]|[0-4][0-9])" +
-            "|1[0-9][0-9]|[1-9]?[0-9]))" +
-            "\\.){3}(?:(2" +
-            "(5[0-5]|[0-4][0-9])" +
-            "|1[0-9][0-9]|[1-9]?[0-9])" +
-            "|[a-z0-9-]*[a-z0-9]:" +
-            "(?:[\\x01-\\x08\\x0b\\x0c" +
-            "\\x0e-\\x1f\\x21-\\x5a\\x53" +
-            "-\\x7f]|\\\\[\\x01-\\x09" +
-            "\\x0b\\x0c\\x0e-\\x7f])+)" +
-            "\\])";
+public class InputValidator
+{
+    private static final String  EMAIL_REGEX   = "(?:[a-z0-9!#$%&'*+/=?^_" +
+                                                 "`{|}~-]+(?:\\" +
+                                                 ".[a-z0-9!#$%&'*+/=?^_" +
+                                                 "`{|}~-]+)*|\"" +
+                                                 "(?:[\\x01-\\x08\\x0b\\x0c" +
+                                                 "\\x0e-\\x1f\\x21\\x23-\\x5b" +
+                                                 "\\x5d-\\x7f]|\\\\[\\x01" +
+                                                 "-\\x09\\x0b\\x0c\\x0e-\\x7f" +
+                                                 "])*\")@(?:(?:[a-z0-9]" +
+                                                 "(?:[a-z0-9-]*[a-z0-9])?\\.)" +
+                                                 "+[a-z0-9](?:[a-z0-9-]*[a-z0" +
+                                                 "-9])?|\\[(?:(?:(2" +
+                                                 "(5[0-5]|[0-4][0-9])" +
+                                                 "|1[0-9][0-9]|[1-9]?[0-9]))" +
+                                                 "\\.){3}(?:(2" +
+                                                 "(5[0-5]|[0-4][0-9])" +
+                                                 "|1[0-9][0-9]|[1-9]?[0-9])" +
+                                                 "|[a-z0-9-]*[a-z0-9]:" +
+                                                 "(?:[\\x01-\\x08\\x0b\\x0c" +
+                                                 "\\x0e-\\x1f\\x21-\\x5a\\x53" +
+                                                 "-\\x7f]|\\\\[\\x01-\\x09" +
+                                                 "\\x0b\\x0c\\x0e-\\x7f])+)" +
+                                                 "\\])";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
-    public static void validateEmail(String email) throws InvalidEmailException {
+    public static void validateEmail(String email) throws InvalidEmailException
+    {
         if (isEmpty(email)) {
             throw new InvalidEmailException("Email cannot be empty");
         }
@@ -44,20 +45,23 @@ class InputValidator {
         }
     }
 
-    public static void validatePassword(String password) throws InvalidPasswordException {
+    public static void validatePassword(String password)
+            throws InvalidPasswordException
+    {
         if (isEmpty(password)) {
             throw new InvalidPasswordException("Password cannot be empty");
         }
     }
 
-    public static void validateName(String name) throws InvalidNameException {
+    public static void validateName(String name) throws InvalidNameException
+    {
         if (isEmpty(name)) {
             throw new InvalidNameException("Name cannot be empty");
         }
     }
 
-    private static boolean isEmpty(String input) {
+    private static boolean isEmpty(String input)
+    {
         return input == null || !input.matches(".*\\S.*");
     }
-
 }
--- app/src/main/java/comp3350/teachreach/presentation/login/TutorLoginActivity.java
+++ app/src/main/java/comp3350/teachreach/presentation/login/TutorLoginActivity.java
@@ -13,6 +13,8 @@ import com.google.android.material.textfield.TextInputLayout;
 import comp3350.teachreach.R;
 import comp3350.teachreach.logic.account.AuthenticationHandler;
 import comp3350.teachreach.logic.account.InputValidator;
+import comp3350.teachreach.logic.exceptions.input.InvalidEmailException;
+import comp3350.teachreach.logic.exceptions.input.InvalidPasswordException;
 import comp3350.teachreach.objects.interfaces.ITutor;
 import comp3350.teachreach.presentation.profile.TutorProfileActivity;
 import comp3350.teachreach.presentation.signup.TutorSignUpActivity;
@@ -52,6 +54,8 @@ public class TutorLoginActivity extends AppCompatActivity
         String email    = etTutorEmail.getText().toString().trim();
         String password = etTutorPassword.getText().toString().trim();
         try {
+            tilTutorEmail.setError(null);
+            tilTutorPassword.setError(null);
             InputValidator.validateEmail(email);
             InputValidator.validatePassword(password);
             ITutor tutor = authenticationHandler.authenticateTutor(email,
@@ -62,6 +66,10 @@ public class TutorLoginActivity extends AppCompatActivity
             intent.putExtra("TUTOR_ID", tutor.getTutorID());
             startActivity(intent);
             finish();
+        } catch (final InvalidEmailException e) {
+            tilTutorEmail.setError(e.getMessage());
+        } catch (final InvalidPasswordException e) {
+            tilTutorPassword.setError(e.getMessage());
         } catch (final Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
