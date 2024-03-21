76e0b26fdbccef005302439e681fee758ea8d560
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-03-21 15:44:37 -0500

    Started mockito stuff, added it to the gradle build. Using mockito 4.0.0

---
+++ app/src/test/java/comp3350/teachreach/tests/logic/mockitoUnitTests/DAOTests/AccessAccountsTest.java
@@ -0,0 +1,10 @@
+package comp3350.teachreach.tests.logic.mockitoUnitTests.DAOTests;
+
+import org.mockito.*;
+
+import comp3350.teachreach.data.interfaces.IAccountPersistence;
+
+public class AccessAccountsTest {
+    @Mock
+    IAccountPersistence accountPersistence;
+}
