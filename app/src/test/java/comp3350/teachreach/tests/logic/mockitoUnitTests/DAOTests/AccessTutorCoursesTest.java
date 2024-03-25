83c12c63a1982363b32ad8fd891c6302fe0517cc
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-03-25 16:36:04 -0500

    Finishing up most of the DAO mock tests

---
+++ app/src/test/java/comp3350/teachreach/tests/logic/mockitoUnitTests/DAOTests/AccessTutorCoursesTest.java
@@ -0,0 +1,47 @@
+package comp3350.teachreach.tests.logic.mockitoUnitTests.DAOTests;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import comp3350.teachreach.data.hsqldb.PersistenceException;
+import comp3350.teachreach.data.interfaces.ITutoredCoursesPersistence;
+import comp3350.teachreach.logic.DAOs.AccessTutoredCourses;
+import comp3350.teachreach.logic.DAOs.DataAccessException;
+import comp3350.teachreach.objects.Course;
+import comp3350.teachreach.objects.interfaces.ICourse;
+
+public class AccessTutorCoursesTest {
+
+    @Mock
+    ITutoredCoursesPersistence tutoredCoursesPersistence;
+
+    @InjectMocks
+    AccessTutoredCourses accessTutoredCourses;
+
+    @Test
+    public void getTutoredCoursesByTutorIDTest() {
+        List<ICourse> returns = new ArrayList<ICourse>();
+
+        returns.add(new Course("COMP1012", "Introduction to Computer Science for Engineers"));
+        returns.add(new Course("COMP 2080", "Analysis of Algorithms"));
+
+        when(tutoredCoursesPersistence.getTutorCourseByTutorID(1)).thenReturn(returns);
+        when(tutoredCoursesPersistence.getTutorCourseByTutorID(2)).thenThrow(new PersistenceException("Error in test, intentional"));
+
+        List<ICourse> results = accessTutoredCourses.getTutoredCoursesByTutorID(1);
+
+        assertEquals("Error in results from getTutoredCoursesByTutorID", results.size(), 2);
+        assertEquals("Error in results from getTutoredCoursesByTutorID", results.get(0).getCourseCode(), "COMP 1012");
+
+        assertThrows("Expected DataAccessException expected but was not thrown", DataAccessException.class, () -> accessTutoredCourses.getTutoredCoursesByTutorID(2));
+
+    }
+}
