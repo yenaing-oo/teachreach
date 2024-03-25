3d14d95e5f2d80bcc54c579456e5916e0e050a8c
hokt <hokt@myumanitoba.ca>
2024-03-25 17:26:20 -0500

    might meed more fixing

--- app/src/androidTest/java/comp3350/teachreach/TeachReachTest.java
+++ app/src/androidTest/java/comp3350/teachreach/TeachReachTest.java
@@ -1,16 +1,5 @@
 package comp3350.teachreach;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import comp3350.teachreach.presentation.login.StudentLoginActivity;
-import comp3350.teachreach.R;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -19,18 +8,34 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import comp3350.teachreach.presentation.MainActivity;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class TeachReachTest {
+public class TeachReachTest
+{
 
     @Rule
-    public ActivityScenarioRule<StudentLoginActivity> activityRule =
-            new ActivityScenarioRule<>(StudentLoginActivity.class);
+    public ActivityScenarioRule<MainActivity> activityRule
+            = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void testSuccessfulLogin() {
-        onView(withId(R.id.tilStudentLoginEmail)).perform(typeText("student@email.com"), closeSoftKeyboard());
-        onView(withId(R.id.tilStudentLoginPassword)).perform(typeText("123"), closeSoftKeyboard());
+    public void testSuccessfulLogin()
+    {
+        onView(withId(R.id.btnStudentLogin)).perform(click());
+
+        onView(withId(R.id.etStudentLoginEmail)).perform(typeText(
+                "student@email.com"), closeSoftKeyboard());
+        onView(withId(R.id.etStudentLoginPassword)).perform(typeText("123"),
+                                                            closeSoftKeyboard());
 
         onView(withId(R.id.btnLoginAsStudent)).perform(click());
 
