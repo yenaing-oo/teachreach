8a1cb0c2acb7d5ae9104c74564278bab00a14ac4
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-03-27 20:44:03 +0000

    Update file AccessStudents.java

--- app/src/main/java/comp3350/teachreach/logic/DAOs/AccessStudents.java
+++ app/src/main/java/comp3350/teachreach/logic/DAOs/AccessStudents.java
@@ -18,7 +18,7 @@ public class AccessStudents {
         students = studentPersistence.getStudents();
     }
 
-    public AccessStudents(IStudentPersistence studentPersistence) {
+    public AccessStudents(final IStudentPersistence studentPersistence) {
         AccessStudents.studentPersistence = studentPersistence;
         students = studentPersistence.getStudents();
     }
@@ -34,7 +34,7 @@ public class AccessStudents {
         if (students == null) {
             students = studentPersistence.getStudents();
         }
-        return students
+        return AccessStudents.students
                 .values()
                 .stream()
                 .filter(s -> s.getAccountID() == studentAccountID)
