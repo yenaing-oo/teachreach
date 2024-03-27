c242d17b6f1576ecf15ffcb306a43a3e9424ceaa
Aidan Labossiere <laboss42@myumanitoba.ca>
2024-03-27 15:18:02 -0500

    Debugging all the mockito unit tests

--- app/src/test/java/comp3350/teachreach/tests/logic/mockitoUnitTests/DAOTests/AccessSessionsTest.java
+++ app/src/test/java/comp3350/teachreach/tests/logic/mockitoUnitTests/DAOTests/AccessSessionsTest.java
@@ -1,10 +1,7 @@
 package comp3350.teachreach.tests.logic.mockitoUnitTests.DAOTests;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-import static comp3350.teachreach.objects.SessionStatus.ACCEPTED;
-import static comp3350.teachreach.objects.SessionStatus.PENDING;
-import static comp3350.teachreach.objects.SessionStatus.REJECTED;
+import static org.mockito.Mockito.doReturn;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,9 +16,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import comp3350.teachreach.application.TRData;
 import comp3350.teachreach.data.interfaces.ISessionPersistence;
 import comp3350.teachreach.logic.DAOs.AccessSessions;
 import comp3350.teachreach.objects.Session;
+import comp3350.teachreach.objects.SessionStatus;
 import comp3350.teachreach.objects.Student;
 import comp3350.teachreach.objects.TimeSlice;
 import comp3350.teachreach.objects.Tutor;
@@ -36,22 +35,25 @@ public class AccessSessionsTest {
     @InjectMocks
     private AccessSessions accessSessions;
 
+
     @Before
     public void init() {
+        TRData.setDefaultEnums();
+
         Map<Integer, ISession> returns = new HashMap<>();
-        returns.put(1, new Session(1, 1,1, new TimeSlice(LocalDateTime.of(2100, 12, 1, 10, 0 ), LocalDateTime.of(2100, 1, 1, 11, 0)), 15, "Coffee Shop", PENDING));
-        returns.put(2, new Session(2, 2,1, new TimeSlice(LocalDateTime.of(2100, 12, 1, 12, 0 ), LocalDateTime.of(2100, 1, 1, 13, 0)), 15, "Coffee Shop", PENDING));
-        returns.put(3, new Session(3, 3,1, new TimeSlice(LocalDateTime.of(2100, 12, 2, 10, 0 ), LocalDateTime.of(2100, 1, 2, 13, 0)), 15, "Library", ACCEPTED));
-        returns.put(4, new Session(4, 4,1, new TimeSlice(LocalDateTime.of(2100, 12, 2, 12, 0 ), LocalDateTime.of(2100, 1, 2, 11, 0)), 15, "Library", ACCEPTED));
-        returns.put(5, new Session(5, 2,1, new TimeSlice(LocalDateTime.of(2100, 12, 3, 12, 0 ), LocalDateTime.of(2100, 1, 1, 13, 0)), 15, "Library", REJECTED));
-        returns.put(6, new Session(6, 6,1, new TimeSlice(LocalDateTime.of(2100, 12, 3, 10, 0 ), LocalDateTime.of(2100, 1, 1, 11, 0)), 15, "Library", REJECTED));
-        returns.put(7, new Session(7, 7,4, new TimeSlice(LocalDateTime.of(2100, 12, 1, 12, 0 ), LocalDateTime.of(2100, 1, 1, 13, 0)), 15, "Coffee Shop", PENDING));
-        returns.put(8, new Session(8, 2,4, new TimeSlice(LocalDateTime.of(2100, 12, 1, 10, 0 ), LocalDateTime.of(2100, 1, 1, 11, 0)), 15, "Coffee Shop", PENDING));
-        returns.put(9, new Session(9, 9,5, new TimeSlice(LocalDateTime.of(2100, 12, 2, 12, 0 ), LocalDateTime.of(2100, 1, 2, 13, 0)), 15, "Library", ACCEPTED));
-        returns.put(10, new Session(10, 10,5, new TimeSlice(LocalDateTime.of(2100, 12, 2, 10, 0 ), LocalDateTime.of(2100, 1, 2, 11, 0)), 15, "Library", ACCEPTED));
-        returns.put(11, new Session(11, 11,6, new TimeSlice(LocalDateTime.of(2100, 12, 3, 12, 0 ), LocalDateTime.of(2100, 1, 1, 13, 0)), 15, "Coffee Shop", REJECTED));
-        returns.put(12, new Session(12, 12,6, new TimeSlice(LocalDateTime.of(2100, 12, 3, 10, 0 ), LocalDateTime.of(2100, 1, 1, 11, 0)), 15, "Coffee Shop", REJECTED));
-        when(sessionPersistence.getSessions()).thenReturn(returns);
+        returns.put(1, new Session(1, 1,1, new TimeSlice(LocalDateTime.of(2100, 12, 1, 10, 0 ), LocalDateTime.of(2100, 12, 1, 11, 0)), 15, "Coffee Shop", SessionStatus.PENDING));
+        returns.put(2, new Session(2, 2,1, new TimeSlice(LocalDateTime.of(2100, 12, 1, 12, 0 ), LocalDateTime.of(2100, 12, 1, 13, 0)), 15, "Coffee Shop", SessionStatus.PENDING));
+        returns.put(3, new Session(3, 3,1, new TimeSlice(LocalDateTime.of(2100, 12, 2, 10, 0 ), LocalDateTime.of(2100, 12, 2, 13, 0)), 15, "Library", SessionStatus.ACCEPTED));
+        returns.put(4, new Session(4, 4,1, new TimeSlice(LocalDateTime.of(2100, 12, 2, 12, 0 ), LocalDateTime.of(2100, 12, 2, 11, 0)), 15, "Library", SessionStatus.ACCEPTED));
+        returns.put(5, new Session(5, 2,1, new TimeSlice(LocalDateTime.of(2100, 12, 3, 12, 0 ), LocalDateTime.of(2100, 12, 1, 13, 0)), 15, "Library", SessionStatus.REJECTED));
+        returns.put(6, new Session(6, 6,1, new TimeSlice(LocalDateTime.of(2100, 12, 3, 10, 0 ), LocalDateTime.of(2100, 12, 1, 11, 0)), 15, "Library", SessionStatus.REJECTED));
+        returns.put(7, new Session(7, 7,2, new TimeSlice(LocalDateTime.of(2100, 12, 1, 12, 0 ), LocalDateTime.of(2100, 12, 1, 13, 0)), 15, "Coffee Shop", SessionStatus.PENDING));
+        returns.put(8, new Session(8, 2,2, new TimeSlice(LocalDateTime.of(2100, 12, 1, 10, 0 ), LocalDateTime.of(2100, 12, 1, 11, 0)), 15, "Coffee Shop", SessionStatus.PENDING));
+        returns.put(9, new Session(9, 9,2, new TimeSlice(LocalDateTime.of(2100, 12, 2, 12, 0 ), LocalDateTime.of(2100, 12, 2, 13, 0)), 15, "Library", SessionStatus.ACCEPTED));
+        returns.put(10, new Session(10, 10,2, new TimeSlice(LocalDateTime.of(2100, 12, 2, 10, 0 ), LocalDateTime.of(2100, 12, 2, 11, 0)), 15, "Library", SessionStatus.ACCEPTED));
+        returns.put(11, new Session(11, 11,2, new TimeSlice(LocalDateTime.of(2100, 12, 3, 12, 0 ), LocalDateTime.of(2100, 12, 1, 13, 0)), 15, "Coffee Shop", SessionStatus.REJECTED));
+        returns.put(12, new Session(12, 12,2, new TimeSlice(LocalDateTime.of(2100, 12, 3, 10, 0 ), LocalDateTime.of(2100, 12, 1, 11, 0)), 15, "Coffee Shop", SessionStatus.REJECTED));
+        doReturn(returns).when(sessionPersistence).getSessions();
 
         accessSessions = new AccessSessions(sessionPersistence);
         MockitoAnnotations.openMocks(this);
@@ -65,7 +67,7 @@ public class AccessSessionsTest {
 
         result = accessSessions.getSessions(new Tutor(2, 2));
 
-        assertEquals("Issue with getSessions for tutor", result.size(), 6);
+        assertEquals("Issue with getSessions for tutor", 6, result.size());
 
         result = accessSessions.getSessions(new Student(2, 3));
 
@@ -82,7 +84,7 @@ public class AccessSessionsTest {
 
         result = accessSessions.getPendingSessions(new Tutor(2, 2));
 
-        assertEquals("Issue with getPendingSessions for tutor", 3, result.size());
+        assertEquals("Issue with getPendingSessions for tutor", 2, result.size());
 
         result = accessSessions.getPendingSessions(new Student(2, 3));
 
@@ -118,7 +120,7 @@ public class AccessSessionsTest {
 
         assertEquals("Issue with getRejectedSessions for tutor", 2, result.size());
 
-        result = accessSessions.getRejectedSessions(new Tutor(2, 2));
+        result = accessSessions.getRejectedSessions(new Student(2, 3));
 
         assertEquals("Issue with getRejectedSessions for student", 1, result.size());
 
--- app/src/test/java/comp3350/teachreach/tests/logic/mockitoUnitTests/DAOTests/AccessTutorAvailabilityTest.java
+++ app/src/test/java/comp3350/teachreach/tests/logic/mockitoUnitTests/DAOTests/AccessTutorAvailabilityTest.java
@@ -1,7 +1,7 @@
 package comp3350.teachreach.tests.logic.mockitoUnitTests.DAOTests;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -36,7 +36,7 @@ public class AccessTutorAvailabilityTest {
     private List<ITimeSlice> returns1 = new ArrayList<>();
     private ITutor tutor1 = new Tutor(1,1);
     private List<ITimeSlice> returns2 = new ArrayList<>();
-    private ITutor tutor2 = new Tutor(1,1);
+    private ITutor tutor2 = new Tutor(2,2);
     private LocalDate date10 = LocalDate.of(2100, 12, 10);
     private LocalDate date11 = LocalDate.of(2100, 12, 11);
     private LocalDate date12 = LocalDate.of(2100, 12, 12);
@@ -55,18 +55,20 @@ public class AccessTutorAvailabilityTest {
         returns2.add(new TimeSlice(LocalDateTime.of(2100, 12, 10, 18, 15), LocalDateTime.of(2100, 12, 10, 20,  30)));
         returns2.add(new TimeSlice(LocalDateTime.of(2100, 12, 13, 19, 0), LocalDateTime.of(2100, 12, 13, 21, 0)));
 
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor1, date10)).thenReturn(returns1.subList(0,1));
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor1, date11)).thenReturn(returns2.subList(1,2));
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor1, date12)).thenReturn(returns1.subList(2,4));
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor1, date13)).thenReturn(returns2.subList(0,0));
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor2, date10)).thenReturn(returns1.subList(0,4));
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor2, date11)).thenReturn(returns2.subList(0,0));
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor2, date12)).thenReturn(returns2.subList(4,5));
-        when(tutorAvailabilityPersistence.getAvailabilityOnDay(tutor2, date13)).thenReturn(returns2.subList(0,0));
+        doReturn(returns1).when(tutorAvailabilityPersistence).getAvailability(tutor1);
+        doReturn(returns2).when(tutorAvailabilityPersistence).getAvailability(tutor2);
+        doReturn(returns1.subList(0,1)).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor1, date10);
+        doReturn(returns2.subList(1,2)).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor1, date11);
+        doReturn(returns1.subList(2,4)).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor1, date12);
+        doReturn(returns2.subList(0,0)).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor1, date13);
+        doReturn(returns1.subList(0,4)).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor2, date10);
+        doReturn(returns2.subList(0,0)).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor2, date11);
+        doReturn(returns2.subList(4,5)).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor2, date12);
+        doReturn(returns2.subList(4, returns2.size())).when(tutorAvailabilityPersistence).getAvailabilityOnDay(tutor2, date13);
 
-
-        MockitoAnnotations.openMocks(this);
         accessTutorAvailability = new AccessTutorAvailability(tutorAvailabilityPersistence);
+        MockitoAnnotations.openMocks(this);
+
     }
     @Test
     public void getAvailabilityTest() {
